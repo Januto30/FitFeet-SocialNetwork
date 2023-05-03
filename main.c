c1c77c26d6ae887924c5be39b35134481c43b778
janpr <jan.prats04@gmail.com>
2023-05-03 12:46:18 +0200

    Commit 2

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include "menu.h"
 #include <stdio.h>
 
 int main() {
---
+++ menu.h
@@ -0,0 +1,6 @@
+#ifndef FITFEET_MENU_H
+#define FITFEET_MENU_H
+#include <stdio.h>
+#define MAX_LENGTH 200
+
+#endif //FITFEET_MENU_H
