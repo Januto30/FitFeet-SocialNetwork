92446f5fa96f36af804076a5fadeeb029ea55eb7
janpr <jan.prats04@gmail.com>
2023-05-03 13:06:06 +0200

    Commit 4

--- menu.c
+++ menu.c
@@ -13,3 +13,50 @@ void menu() {
     printf("=========================================\n");
     printf("Selecciona una opcio: ");
 }
+
+int select_option() {
+    int option;
+    while (1) {
+        if (scanf("%d", &option) != 1) {
+            while (getchar() != '\n');
+        } else if (option >= 1 && option <= 4) {
+            printf("\n");
+            printf("\n");
+            return option;
+        }
+        printf("Opcio incorrecta. Torna a intentar.\n");
+    }
+}
+
+int print_option(int option, user_list *Llista){
+    if (option == 1){
+        User *usuari = (User *) malloc(sizeof(User));
+        printf("------INSERTAR NOU USUARI------\n");
+        emmagatzema_dades(usuari);
+        afegir_usuari(Llista, usuari);
+
+    } else if (option == 2){
+        printf("------LLISTAR TOTS ELS USUARIS EXSISTENTS------\n");
+        print_users(Llista);
+    } else if (option == 3){
+        char usuari[MAX_LENGTH];
+        printf("\n---Quin usuari ets?---\n");
+        print_users(Llista);
+        scanf("%s", usuari);
+        User *current = Llista -> head;
+        while (current != NULL) {
+            if (strcmp(usuari, current -> nom) == 0) {
+                checkPassword(current);
+            }
+            current = current -> next;
+        }
+        printf("\n");
+        printf("---| 1. Enviar solicituds d'amistat   |---\n");
+        printf("---| 2. Gestionar solicituds pendents |---\n");
+        printf("---| 3. Realitzar una publicacio      |---\n");
+        printf("---| 4. Llistar les publicacions      |---\n");
+        printf("\n");
+    } else if (option == 4) {
+        return 1;
+    }
+}
--- menu.h
+++ menu.h
@@ -3,4 +3,32 @@
 #include <stdio.h>
 #define MAX_LENGTH 200
 
+typedef struct {
+    char nom[MAX_LENGTH];
+    char password[MAX_LENGTH];
+    char cognom1[MAX_LENGTH];
+    char cognom2[MAX_LENGTH];
+    int edat;
+    char correu[MAX_LENGTH];
+    char ubi[MAX_LENGTH];
+    char gust1[MAX_LENGTH];
+    char gust2[MAX_LENGTH];
+    char gust3[MAX_LENGTH];
+    char gust4[MAX_LENGTH];
+    char gust5[MAX_LENGTH];
+    struct User* next;
+}User;
+
+typedef struct user_list {
+    User* head;
+    int num_persones;
+} user_list;
+
+void menu();
+
+int select_option();
+
+int print_option(int option, user_list *Llista);
+
+
 #endif //FITFEET_MENU_H
