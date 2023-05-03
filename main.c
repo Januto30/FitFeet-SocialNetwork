34ec32dd45a8ac2cc104f6f75b24c33b97cbf31c
adris <adriana.salinas01@estudiant.upf.edu>
2023-05-03 13:04:14 +0200

    COMMIT 3

--- main.c
+++ main.c
@@ -2,6 +2,15 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int a = 1;
+    user_list Llista;
+    Llista.head = NULL;
+    Llista.num_persones = 0;
+    while (a != 0) {
+        int option1;
+        menu();
+        option1 = select_option();
+        print_option(option1, &Llista);
+    }
     return 0;
 }
--- menu.c
+++ menu.c
@@ -2,3 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+void menu() {
+    printf("=========================================\n");
+    printf("|           Benvingut a Feetfit         |\n");
+    printf("=========================================\n");
+    printf("| 1. Insereix nou usuari                |\n");
+    printf("| 2. Llistar tots els usuaris exsistens |\n");
+    printf("| 3. Iniciar sessio                     |\n");
+    printf("| 4. Sortir                             |\n");
+    printf("=========================================\n");
+    printf("Selecciona una opcio: ");
+}
